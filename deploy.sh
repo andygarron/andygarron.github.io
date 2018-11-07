b3161e48ed5100eef11828920811d3ebbaef3029
AndyG <anderson.garron@gmail.com>
2018-11-06 22:03:46 -0800

    another attempt to deploy properly

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@ git checkout source
 COMMIT_SHA="$(git rev-parse HEAD)"
 yarn build
 git checkout master
+rm -rf static
 mv build/* . # explode the build folder
+rm -r build
 git add .
 git commit -m "Deploy ${COMMIT_SHA}"
 git push
