77116a31df2b665a968c7ab0170ff57e879df069
AndyG <anderson.garron@gmail.com>
2018-11-10 10:52:33 -0800

    removed example code

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,18 @@ import Header from './Header';
 import SocialMediaLink from './SocialMediaLink';
 import GithubIcon from '../icons/GithubIcon';
 import styles from './App.module.styl';
-import { MediaQueryExample } from './examples/MediaQueryExample';
 
 class App extends Component {
   render() {
     return (
-        <MediaQueryExample/>
-      // <div>
-      //   <div className={styles.main}>
-      //     <Header />
-      //   </div>
-      //   <div className={styles.socialLinks}>
-      //     <SocialMediaLink text="Github" href="https://github.com/angarron" icon={GithubIcon} />
-      //   </div>
-      // </div>
+      <div>
+        <div className={styles.main}>
+          <Header />
+        </div>
+        <div className={styles.socialLinks}>
+          <SocialMediaLink text="Github" href="https://github.com/angarron" icon={GithubIcon} />
+        </div>
+      </div>
     );
   }
 }
