497b7812a2295059f7fe7e2eb95998e773d27031
AndyG <anderson.garron@gmail.com>
2019-11-18 19:56:12 -0800

    idk what even tbqh

--- src/components/HoverableImage.js
+++ src/components/HoverableImage.js
@@ -8,15 +8,41 @@ type Props = {
   hoverSrc: string
 };
 
-class HoverableImage extends Component<Props> {
+type State = {
+  isHovered: boolean
+};
+
+class HoverableImage extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { isHovered: false };
+  }
+
   render() {
+    const image: string = this.state.isHovered
+      ? this.props.hoverSrc
+      : this.props.staticSrc;
+
     return (
       <div className={styles.container}>
-        <img className={styles.staticImage} src={this.props.staticSrc} />
-        <img className={styles.hoveredImage} src={this.props.hoverSrc} />
+        <img
+          className={styles.image}
+          src={image}
+          alt=""
+          onMouseEnter={this.onHover}
+          onMouseOut={this.onUnhover}
+        />
       </div>
     );
   }
+
+  onHover = () => {
+    this.setState({ isHovered: true });
+  };
+
+  onUnhover = () => {
+    this.setState({ isHovered: false });
+  };
 }
 
 export default HoverableImage;
