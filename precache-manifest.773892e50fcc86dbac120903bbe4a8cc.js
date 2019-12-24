3e0ea06c1be82eb620d0f777b06a78d33a6cfb56
AndyG <anderson.garron@gmail.com>
2019-12-24 13:32:09 -0500

    Updates

---
+++ precache-manifest.773892e50fcc86dbac120903bbe4a8cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d16698e8beb7aa718c4",
+    "url": "/static/js/main.1d16698e.chunk.js"
+  },
+  {
+    "revision": "ec3413fdf0772accbf2d",
+    "url": "/static/js/1.ec3413fd.chunk.js"
+  },
+  {
+    "revision": "1d16698e8beb7aa718c4",
+    "url": "/static/css/main.671ea33e.chunk.css"
+  },
+  {
+    "revision": "b3ef03e1eeaee869d62bc3d5f5f5503a",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c114e9086ea87284cbe99d05379405c.js"
+  "/precache-manifest.773892e50fcc86dbac120903bbe4a8cc.js"
 );
 
 workbox.clientsClaim();
